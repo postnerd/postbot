2291c773c9195a4bdb924638a2f48bbb81876004
Martin <martin@postnerd.de>
2023-07-07 20:35:24 +0200

    [+] simple evaluation

---
+++ src/evaluate.ts
@@ -0,0 +1,25 @@
+import Board from "./board";
+
+const PIECE_VALUES = {
+	p: -100,
+	n: -300,
+	b: -300,
+	r: -500,
+	q: -900,
+	k: -10000,
+	P: 100,
+	N: 300,
+	B: 300,
+	R: 500,
+	Q: 900,
+	K: 10000,
+};
+
+export default function evaluate(board: Board): number {
+	let score = 0;
+	board.squares.forEach((square) => {
+		if (square.piece) score += PIECE_VALUES[square.piece];
+	});
+
+	return score;
+}
