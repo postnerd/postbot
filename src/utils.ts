3341c40b64d3888af04833603e56517749a00c9e
Martin <martin@postnerd.de>
2023-09-30 22:52:50 +0200

    [u] print board should now work within worker

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
 import Board, { type Move } from "./board";
 import { parentPort } from "worker_threads";
 
-export function printBoardToConsole(board: Board, isDebug: boolean) {
+export function printBoardToConsole(board: Board) {
 	let boardString: string = " —————————————————————————————————\n";
 	for (let i = 20; i < 100; i++) {
 		if (i !== 20 && i % 10 === 0) boardString += " |\n —————————————————————————————————\n";
@@ -11,13 +11,11 @@ export function printBoardToConsole(board: Board, isDebug: boolean) {
 		boardString += ` | ${board.squares[i].piece ?? " "}`;
 	}
 	boardString += " |\n —————————————————————————————————\n";
-	console.log(boardString);
+	communicator.log(boardString);
 
-	if (isDebug) {
-		console.log(`${board.activeColor} to play | Possible moves: ${board.getPossibleMoves().length} | King check: ${board.isCheck()} | Checkmate: ${board.isCheckmate()}`);
-		console.log(`Move: ${board.moveCount} (${board.halfMoveCountSinceLastCaptureOrPawnMove}) | O-O: ${board.castlingInformation.isWhiteKingSidePossible} | O-O-O: ${board.castlingInformation.isWhiteQueenSidePossible} | o-o: ${board.castlingInformation.isBlackKingSidePossible} | o-o-o: ${board.castlingInformation.isBlackQueenSidePossible} | en passant: ${board.enPassantSquarePosition}`);
-		console.log(`Current Hash: ${board.hash}`);
-	}
+	communicator.log(`${board.activeColor} to play | Possible moves: ${board.getPossibleMoves().length} | King check: ${board.isCheck()} | Checkmate: ${board.isCheckmate()}`);
+	communicator.log(`Move: ${board.moveCount} (${board.halfMoveCountSinceLastCaptureOrPawnMove}) | O-O: ${board.castlingInformation.isWhiteKingSidePossible} | O-O-O: ${board.castlingInformation.isWhiteQueenSidePossible} | o-o: ${board.castlingInformation.isBlackKingSidePossible} | o-o-o: ${board.castlingInformation.isBlackQueenSidePossible} | en passant: ${board.enPassantSquarePosition}`);
+	communicator.log(`Current Hash: ${board.hash}`);
 }
 
 export const communicator = {
